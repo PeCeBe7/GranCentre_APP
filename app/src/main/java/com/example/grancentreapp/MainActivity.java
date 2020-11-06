4e6e4c27aeba18e9be474334e231e0d1075cd14b
NET Barcelona <sepulvedagarciaalex@gmail.com>
2020-11-06 19:59:57 +0100

    Segundo commit

--- app/src/main/java/com/example/grancentreapp/MainActivity.java
+++ app/src/main/java/com/example/grancentreapp/MainActivity.java
@@ -2,13 +2,37 @@ package com.example.grancentreapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    String id;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button bt_restaurant = findViewById(R.id.bt_restaurants);
+        Button bt_hotel = findViewById(R.id.bt_hotels);
+        bt_restaurant.setOnClickListener(this);
+        bt_hotel.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Button bt = (Button) v;
+        id=getResources().getResourceEntryName(bt.getId());
+        switch(id)
+        {
+            case "bt_restaurants":
+                //Intent intentR = new Intent(this,MainRestaurant.class);
+                //startActivity(intentR);
+                ;break;
+            case "bt_hotels":
+               // Intent intentH = new Intent(this,MainHotels.class);
+                //startActivity(intentH);
+                ;break;
+        }
     }
 }
